90291b089cfa48a8b96907048094dfc62ddf018b
Hashim-mirjan <hashimmirjan1@gmail.com>
2026-04-24 08:52:34 +0100

    Adding comparison step

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
 from src.data import load_dataset, show_sample_images, create_dataloaders
-from src.model import build_alexnet, get_loss_function, get_optimizer
+from src.model import build_model, get_loss_function, get_optimizer
 from src.train import train_and_validate_nn
 from src.utils import plot_accuracy, plot_confusion_matrix
 
 from torchsummary import summary
 
-EPOCHS = 16
+EPOCHS = 15
+MODEL_NAME = "googlenet"   # "alexnet" or "googlenet"
+LEARNING_RATE = 0.01      # try 0.0001, 0.001, or 0.01
 
 def main():
     datastore = load_dataset()
     class_names = datastore.classes
 
-    show_sample_images(datastore)
+    #show_sample_images(datastore)
 
     train_set, val_set, train_loader, val_loader = create_dataloaders(datastore)
 
     print("Validation set length:", len(val_set))
+    print("Model:", MODEL_NAME)
+    print("Learning rate:", LEARNING_RATE)
 
-    model = build_alexnet(len(class_names))
+    model = build_model(MODEL_NAME, len(class_names))
 
     summary(model, (3, 227, 227))
 
     loss_fn = get_loss_function()
-    optimizer = get_optimizer(model)
+    optimizer = get_optimizer(model, LEARNING_RATE)
 
     train_acc, val_acc, preds, actuals = train_and_validate_nn(
         model,
--- src/model.py
+++ src/model.py
@@ -2,26 +2,33 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import models
 
-LEARNING_RATE = 0.0001
+def build_model(model_name, num_classes):
+    if model_name == "alexnet":
+        model = models.alexnet(pretrained=True)
 
-def build_alexnet(num_classes):
-    alexnet = models.alexnet(pretrained=True)
+        for param in model.features.parameters():
+            param.requires_grad = False
 
-    # Freeze convolutional feature extractor
-    for param in alexnet.features.parameters():
-        param.requires_grad = False
+        model.classifier[6] = nn.Linear(4096, num_classes)
 
-    # Replace final classifier layer
-    alexnet.classifier[6] = nn.Linear(4096, num_classes)
+    elif model_name == "googlenet":
+        model = models.googlenet(pretrained=True)
 
-    return alexnet
+        for param in model.parameters():
+            param.requires_grad = False
+
+        model.fc = nn.Linear(1024, num_classes)
+
+    else:
+        raise ValueError("Model not supported. Choose 'alexnet' or 'googlenet'.")
+
+    return model
 
 def get_loss_function():
     return nn.CrossEntropyLoss()
 
-def get_optimizer(model):
-    # Only optimize parameters that are trainable
+def get_optimizer(model, learning_rate):
     return optim.SGD(
         filter(lambda p: p.requires_grad, model.parameters()),
-        lr=LEARNING_RATE
+        lr=learning_rate
     )
